17a6a767c83bb78a20c7181ab3f81d9fd11d9341
aryanmaurya066 <76197201+aryanmaurya066@users.noreply.github.com>
2025-07-20 20:13:42 +0530

    removing frontend folder

--- frontend/src/components/Spinner.js
+++
@@ -1,8 +0,0 @@
-export default function Spinner() {
-  return (
-    <div className="text-center my-6">
-      <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-indigo-600 mx-auto"></div>
-      <p className="text-gray-500 mt-2">Analyzing...</p>
-    </div>
-  );
-}
